31d19192c7d55fbb7fca9552c9177c6f18512f20
ramher101 <ramher101@gmail.com>
2017-03-24 00:21:16 +0800

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
     before_action :find_event, only: [:show, :edit, :update, :destroy]
-before_action :authenticate_user!, except: [:index , :show]
+before_action :authenticate_user!, except: [:index , :show, :eventlist ]
   def index
     @events = Event.all
   end
